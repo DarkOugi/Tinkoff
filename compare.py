964ac3943987879c16dbbe77deef128d79ae2b80
Ougi <demon_284@bk.ru>
2023-01-06 17:00:07 +0500

    ready to study ml

--- compare.py
+++ compare.py
@@ -2,8 +2,8 @@ from dist_lev import lev
 import sys
 
 
-def loss_metric(str_one, str_two):
-    return lev(str_one, str_two) / len(str_two)
+def lev_metric(str_one, str_two):
+    return (len(str_one) - lev(str_one, str_two)) / len(str_one)
 
 
 if __name__ == '__main__':
@@ -11,12 +11,11 @@ if __name__ == '__main__':
     files_path = open(read).read().split()
     i = 0
     lev_size = ''
-    while i < len(files_path):
-        lev_size += str(loss_metric(list(open(files_path[i], encoding='UTF-8').read()),
-                                     list(open(files_path[i + 1], encoding='UTF-8').read()))) + '\n'
-        i += 2
+    for i in range(0, len(files_path), 2):
+        string_one = ''.join(open(files_path[i], encoding='UTF-8').read().split())
+        string_two = ''.join(open(files_path[i + 1], encoding='UTF-8').read().split())
+        lev_size += str(lev_metric(string_one, string_two)) + '\n'
 
     file_write = open(write, 'w')
     file_write.write(lev_size)
     file_write.close()
-
